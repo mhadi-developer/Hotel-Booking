d429d7743f303deaf064fc87af273b84ae41543b
Muhammad Hadi <hadiquershi08@gmail.com>
2026-07-05 17:53:24 +0500

    Updated Server.js

--- Backend/server.js
+++ Backend/server.js
@@ -84,7 +84,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options("/*", cors(corsOptions)); // Handle global preflight OPTIONS requests
+app.options("/:splat*", cors(corsOptions)); // Handle global preflight OPTIONS requests
 
 // Parsing and Limiting Middlewares
 app.use(limiter);
