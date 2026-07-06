49a69d2745747e0a67aec8cbff5fd330e01f9021
Muhammad Hadi <hadiquershi08@gmail.com>
2026-07-06 15:32:54 +0500

    CI/CD final Test

--- Backend/server.js
+++ Backend/server.js
@@ -84,7 +84,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options("*", cors()); // Handle global preflight OPTIONS requests
+app.options(/.*/, cors()); // Handle global preflight OPTIONS requests
 
 // Parsing and Limiting Middlewares
 app.use(limiter);
